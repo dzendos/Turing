63f2581d508f6ad97305efe5c29fab129b5812e6
Evgeny <e.gerasimov@mail.com>
2022-09-08 09:47:29 +0300

    game state

--- game_state/game_state.go
+++ game_state/game_state.go
@@ -1,1 +1,22 @@
 package game_state
+
+import (
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+type PlayerRole int
+
+const (
+	Host PlayerRole = iota + 1
+	PlayerA
+	PlayerB
+)
+
+type Player struct {
+	UserId tb.User
+	Role   PlayerRole
+}
+
+type GameState struct {
+	Players [3]Player
+}
